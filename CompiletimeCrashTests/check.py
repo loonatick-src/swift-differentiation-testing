dafd0b8ffdebbaf1947957edfc84296b9c5ed335
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-03-04 17:51:42 +0100

    feat: include reproducer status (crash vs error vs expected error vs ok)

--- CompiletimeCrashTests/check.py
+++ CompiletimeCrashTests/check.py
@@ -1,8 +1,48 @@
 #!/usr/bin/env python
 from sys import argv, stdin, stderr
+from enum import Enum
+from typing import Self
+import re
+
+class ReproducerType(Enum):
+    OK = 0      # should build successfully
+    XERROR = 1  # expected to throw compilation error with proper diagnostics
+    CRASH = 2   # crashes
+    ERROR = 3   # should build successfully, but throws compilation error
+
+    @staticmethod
+    def parse(header: str) -> type[Self | None]:
+        match = re.match(r"^#\s*(OK|ERROR|CRASH|XERROR)\b", header)
+        if match:
+            return ReproducerType[match.group(1)]
+        return None
+
 
 def errprint(*args, **kwargs):
     print(*args, **kwargs, file=stderr)
+
+def check(expected: [str], found: [str]):
+    reproducer_type = ReproducerType.parse(expected[0])
+    if reproducer_type:
+        expected = expected[1:]
+    if not expected:
+        if not found:
+            exit(0)
+        errprint(f"FAIL: expected no output, found {found}")
+        exit(1)
+    idx = 0
+    expected_line = expected[idx]
+    for line in found:
+        if expected_line in line:
+            idx += 1
+            if idx == len(expected):
+                exit(0)
+            expected_line = expected[idx]
+        if idx == len(expected):
+            exit(0)
+    errprint(f"FAIL: \"{expected_line} not found in {found}\"")
+    exit(2)
+
 
 def main():
     expected_output = []
@@ -12,24 +52,13 @@ def main():
             while line := file.readline():
                 expected_output.append(line.rstrip())
         assert(len(expected_output) > 0)
-    output = stdin.readlines() 
+    output = stdin.readlines()
     if not expected_output:
         if not output:
             exit(0)
         errprint(f"FAIL: expected no output, found {output}")
         exit(1)
-    idx = 0
-    expected_line = expected_output[idx]
-    for line in output:
-        if expected_line in line:
-            idx += 1
-            if idx == len(expected_output):
-                exit(0)
-            expected_line = expected_output[idx]
-        if idx == len(expected_output):
-            exit(0)
-    errprint(f"FAIL: \"{expected_output[idx]}\" not found in the output") 
-    exit(2)
+    check(expected_output, output)
 
 if __name__ == '__main__':
     main()
--- CompiletimeCrashTests/run-compiletime-crash-tests.sh
+++ CompiletimeCrashTests/run-compiletime-crash-tests.sh
@@ -2,23 +2,52 @@
 echo -e "Running Compiletime Crash tests\n"
 
 echo -e "Compiletime Crash tests\n"
+
+get_reproducer_type() {
+    local file="$1"
+    local first_line
+    if [[ ! -e $file ]]; then
+        echo "OK"
+        return 0
+    fi
+    first_line=$(head -n 1 "$file" 2>/dev/null)
+    if [[ $first_line =~ ^#[[:space:]]*(OK|ERROR|CRASH|XERROR) ]]; then
+        # capture groups not working?
+        # echo ="${BASH_REMATCH[1]}"
+        echo `echo $first_line | sed 's/^[[:space:]]*# *//;s/[[:space:]]*$//'`
+    else
+        echo "ERROR: No header match"
+        exit 1
+    fi
+}
+
+total_count=0
+fail_count=0
+
 for folder in $(find . -type d -mindepth 1 -maxdepth 1); do
+    let total_count+=1
     cd "$folder" # navigate to current testing folder
     echo "Building and checking output of $folder"
     source ./build.sh # is expected to set RETURN_CODE variable
     echo "Finished building $folder"
+    reproducer_type=$(get_reproducer_type "expected-6.0.3.txt")
     cd - > /dev/null # navigate back to previous folder
-    
     # script expected to succeed
     if [ $RETURN_CODE -eq 0 ]; then
         # script succeeded
-        echo -e "$folder passed as expected\n"
+        echo -e "$folder: $reproducer_type (expected)\n"
+        if [[ "$reproducer_type" = "ERROR" || "$reproducer_type" = "CRASH" ]]; then
+            fail_count=$((fail_count + 1))
+        fi
     else
         # script failed unexpectedly
-        echo -e "$folder failed unexpectedly\n"
+        echo -e "$folder: unexpected output\n"
         exit 1
     fi
 done
+
+echo $total_count
+echo $fail_count
 
 echo "Finished running all Compiletime Crash tests"
 
